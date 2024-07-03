410614bbe299573930227724c02648f9141caadc
Dra-Tammer <2816677369@qq.com>
2024-07-03 11:50:30 +0800

    主页高度的修改

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,18 @@
   <router-view></router-view>
 </template>
 
-<style scoped>
+<!--对app的样式配置不用scoped，要不然无法生效，无法透传到子组件-->
+<style>
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  height: 100%;
+  overflow: hidden;
+}
 
 </style>
